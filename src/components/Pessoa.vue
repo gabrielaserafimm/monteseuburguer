7a45b7c7832ff5fc03cc5b4cc9a0857696caf9c4
Gabriela C Serafim <gabrielacamiloserafim@gmail.com>
2022-06-13 12:41:52 -0300

    Initial commit

---
+++ src/components/Pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Descrição da pessoa:</h2>
+        <Info/>
+    </div>
+</template>
+
+<script>
+import Info from './Info'
+
+    export default {
+        name: 'Pessoa',
+        components: {
+            Info
+        },
+        data() {
+            return {
+                nome: 'Gabriela'
+            }
+        }    
+    }
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }}, minha profissão é {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "Gabriela",
+                profissao: "Desenvolvedora Frontend"
+            }
+        }
+    }
+</script>
